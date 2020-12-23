586cfe99a92c7df5071074e556cd995ef8339f9f
StanislavSorochak <75738611+StanislavSorochak@users.noreply.github.com>
2020-12-23 13:28:52 +0200

    first master commit

---
+++ UnitTest9.1.1/UnitTest9.1.1.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba9.1.1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest911
+{
+	TEST_CLASS(UnitTest911)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int kil_student = 2;
+			student* s = new student[kil_student];
+			s[0].prizv = "клоун";
+			s[0].specialization = Special(4);
+			s[0].phisuka = 5;
+			s[0].matematic = 4;
+			s[0].informatic = 5;
+			s[1].prizv = "візуал";
+			s[1].specialization = Special(3);
+			s[1].phisuka = 4;
+			s[1].matematic = 5;
+			s[1].informatic = 5;
+
+			double l = find_studend(s, kil_student);
+			Assert::AreEqual(1, 1);
+		}
+	};
+}
---
+++ laba9.1.1/Source.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <Windows.h> 
+
+using namespace std;
+
+enum Special { АВ, БІ, ІК, МН, ТР };
+
+string s_special[] = { "Авіоніка",
+					   "Біомедична інженерія",
+					   "Інформаційно-комунікаційні системи",
+					   "Мікро- та наносиситемна техніка",
+					   "Тклекомунікаціїї та радіотехніка" };
+
+struct student
+{
+	string prizv;
+	int kurs;
+	Special specialization;
+	int phisuka;
+	int matematic;
+	int informatic;
+};
+
+void Create(student* s, const int kilkist);
+void Print(student* s, const int kilkist);
+int find_studend(student* s, const int kilkist);
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int kil_student;
+	cout << "Введіть кількість студентів: "; cin >> kil_student;
+	cout << endl;
+	student* s = new student[kil_student];
+
+	Create(s, kil_student);
+	cout << endl;
+	Print(s, kil_student);
+	cout << endl;
+
+
+	cout << "Прізвища студентів, які отримали з фізики оцінки '5': " << endl;
+	if (find_studend(s, kil_student));
+	else
+		cout << "Прізвища студентів, які отримали з фізики оцінки '5' не знайшлося: " << endl << endl;
+
+	delete[] s;
+}
+
+void Create(student* s, const int kilkist)
+{
+	int kurs, special;
+	for (int i = 0; i < kilkist; i++)
+	{
+		cout << endl;
+		cout << "студент № " << i + 1 << endl;
+
+		cin.get();
+		cin.sync();
+
+		cout << "  прізвище: "; getline(cin, s[i].prizv);
+		cout << "  курс: "; cin >> s[i].kurs;
+		cout << "  спеціальність (0 - Авіоніка, 1 - Біомедична інженерія, 2 - Інформаційно-комунікаційні системи, 3 - Мікро- та наносиситемна техніка, 4 - Тклекомунікаціїї та радіотехніка ): "; cin >> special;
+		s[i].specialization = (Special)special;
+		cout << "  оцінка з фізики: "; cin >> s[i].phisuka;
+		cout << "  оцінка з математики: "; cin >> s[i].matematic;
+		cout << "  оцінка з інформатики: "; cin >> s[i].informatic;
+	}
+}
+
+void Print(student* s, const int kilkist)
+{
+	cout << "===================================================================================================="
+		<< endl;
+	cout << "| №  | Прізвище     | Курс | Спеціальність                    | Фізика | Математика  | Інформатика |"
+		<< endl;
+	cout << "-----------------------------------------------------------------------------------------" << endl;
+	for (int i = 0; i < kilkist; i++)
+	{
+		cout << "| " << setw(2) << right << i + 1 << " "
+			<< "| " << setw(13) << left << s[i].prizv
+			<< "| " << setw(3) << right << s[i].kurs << "  "
+			<< "| " << setw(33) << left << s_special[s[i].specialization] << ""
+			<< "| " << setw(4) << right << s[i].phisuka << "   "
+			<< "| " << setw(6) << right << s[i].matematic << "      "
+			<< "| " << setw(6) << right << s[i].informatic << "      |" << endl;
+	}
+	cout << "====================================================================================================" << endl;
+}
+
+
+int find_studend(student* s, const int kilkist)
+{
+	int l = 0;
+	for (int i = 0; i < kilkist; i++)
+		if (s[i].phisuka > 3)
+		{
+			cout << "№ " << i + 1 << " " << s[i].prizv << " --- з фізики " << s[i].phisuka << endl;
+			l = 1;
+		}
+
+	return l;
+}
